0fadd8e3888ecadf592f51702cb9813b1750cdfe
jimmy yang <jimmyyang5296@gmail.com>
2017-03-07 10:36:59 +0800

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ resources :photos
+  # Sample of regular route:
+  #   match 'products/:id' => 'catalog#view'
+  # Keep in mind you can assign values other than :controller and :action
+
+end
